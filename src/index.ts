9d67638fbc83c5de8dc09acddd7e2218f46eb1d2
muryoh <muryoh@gmail.com>
2021-07-30 19:14:39 +0000

    First commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+function parseMatter(s: string): any {
+  if (s.trim().startsWith("{")) {
+    // JSON
+    return JSON.parse(s);
+  }
+  // TODO support YAML
+  return {};
+}
+
+const matter = (
+  content: string,
+  regexp: RegExp
+): { data?: any; content: string } => {
+  const match = regexp.exec(content);
+  if (match && match[1]) {
+    let data = {};
+    try {
+      data = parseMatter(match[1]);
+      const slicedContent = content.slice(match[0].length);
+      return { data, content: slicedContent };
+    } catch (e) {
+      return {
+        content: ["```js", e.toString(), "```", content].join("\n"),
+      };
+    }
+  }
+  return { content };
+};
+
+const matterRegexp = (begin: string, end: string): RegExp =>
+  new RegExp(`^${begin}(?:json)?(?=\\n)(.*?)\\n${end}\\n`, "s");
+
+export const matterHTML = (content: string): { data?: any; content: string } =>
+  matter(content, matterRegexp("<!--", "-->"));
+export const matterMD = (content: string): { data?: any; content: string } =>
+  matter(content, matterRegexp("---", "---"));
